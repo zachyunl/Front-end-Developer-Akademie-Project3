2c5a69f98a0f8dd8a323375a734e3a02f51001f4
zachyunl <jirizachar@seznam.cz>
2023-03-16 20:57:48 +0100

    show users

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
+export {};
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const url = "https://jsonplaceholder.typicode.com/users";
+
+async function getUsers(): Promise<User[]> {
+  const response = await fetch(url);
+  const users = await response.json();
+  return users;
+}
+
+function renderUsers(users: User[]): void {
+  const userList = document.createElement("ul");
+
+  users.forEach((user) => {
+    const userItem = document.createElement("li");
+    userItem.textContent = `${user.name} (${user.email})`;
+    userList.appendChild(userItem);
+  });
+
+  document.body.appendChild(userList);
+}
+
+getUsers()
+  .then((users) => renderUsers(users))
+  .catch((error) => console.error(error));
