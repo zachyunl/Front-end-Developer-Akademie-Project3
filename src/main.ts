9744b06d3b981222166222974f65f5af10f3cdcd
zachyunl <jirizachar@seznam.cz>
2023-04-02 09:49:46 +0200

    textové pole

--- src/main.ts
+++ src/main.ts
@@ -14,18 +14,37 @@ async function getUsers(): Promise<User[]> {
   return users;
 }
 
-function renderUsers(users: User[]): void {
+function renderUsers(users: User[], searchValue: string): void {
   const userList = document.createElement("ul");
 
-  users.forEach((user) => {
-    const userItem = document.createElement("li");
-    userItem.textContent = `${user.name} (${user.email})`;
-    userList.appendChild(userItem);
-  });
+  users
+    .filter((user) =>
+      searchValue ? user.name.toLowerCase().includes(searchValue.toLowerCase()) : true
+    )
+    .forEach((user) => {
+      const userItem = document.createElement("li");
+      userItem.textContent = `${user.name} (${user.email})`;
+      userList.appendChild(userItem);
+    });
 
   document.body.appendChild(userList);
 }
 
-getUsers()
-  .then((users) => renderUsers(users))
-  .catch((error) => console.error(error));
+const searchInput = document.createElement("input");
+searchInput.type = "text";
+searchInput.placeholder = "Vyhledat uživatele";
+document.body.appendChild(searchInput);
+
+let userList: HTMLUListElement;
+
+getUsers().then((users) => {
+  renderUsers(users, "");
+
+  userList = document.querySelector("ul");
+
+  searchInput.addEventListener("input", () => {
+    const searchValue = searchInput.value.trim();
+    userList.remove();
+    getUsers().then((users) => renderUsers(users, searchValue));
+  });
+});
